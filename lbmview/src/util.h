e4afb59f22b32e36e47f892b4995d109d9fc06fa
a dinosaur <dino@a-dinosaur.com>
2024-06-18 18:22:52 +1000

    lbmview: rewrote endian detection

--- lbmview/src/util.h
+++ lbmview/src/util.h
@@ -3,21 +3,53 @@
 
 // Detect endianess
 
-#ifdef HAVE_ARM_ENDIAN_H
-# include <arm/endian.h>
-#elif defined(HAVE_ENDIAN_H)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+# include <machine/endian.h>
+#elif defined(__linux__) || defined(__CYGWIN__) || defined(__OpenBSD__)
 # include <endian.h>
-# define BYTE_ORDER    __BYTE_ORDER
-# define LITTLE_ENDIAN __LITTLE_ENDIAN
-# define BIG_ENDIAN    __BIG_ENDIAN
+#elif defined(__NetBSD__) || defined(BSD)
+# include <sys/endian.h>
+#elif defined(__GNUC__)
+# include <sys/param.h>
 #else
-# define LITTLE_ENDIAN 1234
-# define BIG_ENDIAN    4321
-# if defined(__APPLE__) || defined(_WIN32)
-#  define BYTE_ORDER LITTLE_ENDIAN
+# if defined(__ORDER_LITTLE_ENDIAN__)
+#  define LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
 # else
-#  error Not implemented
+#  define LITTLE_ENDIAN 1234
 # endif
+# if defined(__ORDER_BIG_ENDIAN__)
+#  define BIG_ENDIAN    __ORDER_BIG_ENDIAN__
+# else
+#  define BIG_ENDIAN    4321
+# endif
+# if (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == LITTLE_ENDIAN) || defined(__LITTLE_ENDIAN__)
+#  define BYTE_ORDER LITTLE_ENDIAN
+# elif (defined(__BYTE_ORDER__) && __BYTE_ORDER__ == BIG_ENDIAN) || defined(__BIG_ENDIAN__)
+#  define BYTE_ORDER BIG_ENDIAN
+# elif defined(_MSC_VER) && (defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(_XBOX))
+#  ifdef _X360
+#   define BYTE_ORDER BIG_ENDIAN
+#  elif defined(_M_I86) || defined(_M_IX86) || defined(_M_X64) || defined(_M_AMD64) || defined(_M_IA64) \
+     || defined(_M_ARM) || defined(_M_ARM64) || defined(_M_PPC) || defined(_M_MIPS) || defined(_M_ALPHA)
+#   define BYTE_ORDER LITTLE_ENDIAN
+#  endif
+# elif defined(__MIPSEL__) || defined(__MIPSEL) || defined(_MIPSEL) \
+    || defined(__ARMEL__) || defined(__THUMBEL__) || defined(__AARCH64EL__) \
+    || defined(__i386__) || defined(__i386) || defined(__X86__) || defined(__I86__) || defined(__386) \
+    || defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) \
+    || defined(__INTEL__) || defined(__itanium__)
+#  define BYTE_ORDER LITTLE_ENDIAN
+# elif defined(__MIPSEB__) || defined(__MIPSEB) || defined(_MIPSEB) \
+    || defined(__ARMEB__) || defined(__THUMBEB__) || defined(__AARCH64EB__) \
+    || defined(__m68k__) || defined(mc68000) || defined(M68000) || defined(__MC68K__) || defined(_M_M68K) \
+    || defined(__PPCGECKO__) || defined(__PPCBROADWAY__) || defined(_XENON) \
+    || defined(__hppa__) || defined(__hppa) || defined(__HPPA__) || defined(__sparc_v8__) || defined(__sparcv8)
+#  define BYTE_ORDER BIG_ENDIAN
+# endif
+#endif
+
+#if !defined(BYTE_ORDER) || !(BYTE_ORDER == LITTLE_ENDIAN || BYTE_ORDER == BIG_ENDIAN)
+# error "Unsupported machine"
 #endif
 
 // Inline control
@@ -59,17 +91,19 @@ static inline uint16_t FORCE_INLINE swap16(uint16_t v) { return v << 8 | v >> 8;
 #endif
 
 #if BYTE_ORDER == LITTLE_ENDIAN
+# define SWAP_LE16(V) (V)
+# define SWAP_LE32(V) (V)
 # define SWAP_BE32(V) swap32(V)
 # define SWAP_BE16(V) swap16(V)
-#else
+# define FOURCC(A, B, C, D) ((uint32_t)(A) | (uint32_t)(B) << 8 | (uint32_t)(C) << 16 | (uint32_t)(D) << 24)
+#elif BYTE_ORDER == BIG_ENDIAN
+# define SWAP_LE32(V) swap32(V)
+# define SWAP_LE16(V) swap16(V)
 # define SWAP_BE32(V) (V)
 # define SWAP_BE16(V) (V)
-#endif
-
-#if BYTE_ORDER == BIG_ENDIAN
 # define FOURCC(A, B, C, D) ((uint32_t)(D) | (uint32_t)(C) << 8 | (uint32_t)(B) << 16 | (uint32_t)(A) << 24)
 #else
-# define FOURCC(A, B, C, D) ((uint32_t)(A) | (uint32_t)(B) << 8 | (uint32_t)(C) << 16 | (uint32_t)(D) << 24)
+# error Not implemented
 #endif
 
 // Maths utilities
