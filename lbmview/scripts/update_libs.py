506ebf8863fd2c7f5608f47d9ffee7b4edc5eeda
a dinosaur <dino@a-dinosaur.com>
2025-05-16 07:33:38 +1000

    lbmview: Update libs

--- lbmview/scripts/update_libs.py
+++ lbmview/scripts/update_libs.py
@@ -2,12 +2,16 @@
 
 # update_libs.py - Public domain
 
+import sys
 from collections import namedtuple
 from urllib.request import urlopen
 from urllib.error import URLError
+from pathlib import Path
 
 
 def main():
+	root = Path(sys.argv[0]).resolve().parent.parent
+
 	File = namedtuple("File", ["repo", "rpath", "dstpath"])
 	files = [
 		File("nothings/stb", "stb_vorbis.c", "src/stb_vorbis.h"),
@@ -18,10 +22,10 @@ def main():
 		remote = f"https://raw.githubusercontent.com/{file.repo}/master/{file.rpath}"
 		try:
 			with urlopen(remote) as request, \
-					open(file.dstpath, "w", newline="\n") as out:
+					root.joinpath(file.dstpath).open("w", encoding="utf-8", newline="\n") as out:
 				out.write(request.read().decode("UTF-8").replace("\r\n", "\n"))
 		except URLError as e:
-			exit(f"Failed to fetch \"{remote}\": {e}")
+			sys.exit(f"Failed to fetch \"{remote}\": {e}")
 
 
 if __name__ == "__main__":
